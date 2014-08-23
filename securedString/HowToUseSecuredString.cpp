e9ffad286a43e19a8e7763b406340f0936a069c8
David M.F. Lai <davidmflai.git@outlook.com>
2014-08-23 20:41:16 +0800

    Initial commit

---
+++ securedString/HowToUseSecuredString.cpp
@@ -0,0 +1,72 @@
+// securedString.cpp : Defines the entry point for the console application.
+//
+
+#include "securedString.h"
+#include <string>
+
+#include "gtest\gtest.h"
+#include "gmock/gmock.h"
+
+
+class HowToUseSecuredStringTest : public ::testing::Test {
+protected:
+	wchar_t *rawString;
+	wchar_t *rawString2;
+	HowToUseSecuredStringTest()
+	{
+		wchar_t tmp[8] = L"rawdata";
+		rawString = new wchar_t[sizeof(tmp)];
+		memcpy(rawString, tmp, sizeof(tmp));
+
+		wchar_t tmp2[9] = L"rawdata2";
+		rawString2 = new wchar_t[sizeof(tmp2)];
+		memcpy(rawString2, tmp2, sizeof(tmp2));
+	}
+};
+
+TEST_F(HowToUseSecuredStringTest, Contruction)
+{
+	SecuredString<wchar_t> securedString{ rawString };
+	ASSERT_THAT(rawString, ::testing::Ne(securedString.c_str()));
+}
+
+TEST_F(HowToUseSecuredStringTest, GainControl)
+{
+	SecuredString<wchar_t> securedString;
+	securedString.gainControl(rawString);
+	ASSERT_THAT(rawString, ::testing::Eq(securedString.c_str()));
+}
+
+TEST_F(HowToUseSecuredStringTest, compare)
+{
+	SecuredString<wchar_t> securedString1{ rawString };
+	SecuredString<wchar_t> securedString2{ rawString };
+	ASSERT_THAT(securedString1 == securedString2, ::testing::Eq(true));
+
+	SecuredString<wchar_t> securedString3{ rawString2 };
+	ASSERT_THAT(securedString1 == securedString3, ::testing::Eq(false));
+}
+
+TEST_F(HowToUseSecuredStringTest, append)
+{
+	SecuredString<wchar_t> securedString1{ rawString };
+	SecuredString<wchar_t> securedString2{ rawString2 };
+
+	securedString1 += securedString2;
+
+	std::wstring mergedstring{ std::wstring{ rawString } +std::wstring{ rawString2 } };
+	std::wstring mergedsecuredstring = securedString1.c_str();
+	ASSERT_THAT(mergedstring == mergedsecuredstring, ::testing::Eq(true));
+}
+
+TEST_F(HowToUseSecuredStringTest, concatenate)
+{
+	SecuredString<wchar_t> securedString1{ rawString };
+	SecuredString<wchar_t> securedString2{ rawString2 };
+
+	SecuredString<wchar_t> securedString3 = securedString1 + securedString2;
+	
+	std::wstring mergedstring{ std::wstring{ rawString } + std::wstring{ rawString2 } };
+	std::wstring mergedsecuredstring = securedString3.c_str();
+	ASSERT_THAT(mergedstring == mergedsecuredstring, ::testing::Eq(true));
+}
---
+++ securedString/securedString.h
@@ -0,0 +1,199 @@
+#ifndef SECUREDSTRING_H
+#define SECUREDSTRING_H
+
+#include <Windows.h>
+
+template <typename T>
+class SecuredString
+{
+public:
+	template<typename T2>
+	friend void swap(SecuredString<T2> &, SecuredString<T2> &);
+
+private:
+	T *data;
+	unsigned int datacnt;
+	bool owningData;
+	bool comparememory(void *lhs, void *rhs, unsigned int length) const;
+
+public:
+	SecuredString();
+	SecuredString(T *data);
+	SecuredString(const SecuredString &);
+	SecuredString(SecuredString &&);
+
+	void gainControl(T *data);
+
+	~SecuredString();
+	bool operator==(const SecuredString &other) const;
+	SecuredString &operator=(SecuredString);
+	SecuredString &operator+=(const SecuredString &rhs);
+	const SecuredString operator+(const SecuredString &other) const;
+
+	T *c_str() const;
+	T *release();
+
+	//for testing use
+	unsigned int __get_length();
+};
+
+template <typename T>
+bool SecuredString<T>::comparememory(void *lhs, void *rhs, unsigned int length) const
+{
+	for (unsigned int counter = 0; counter < length; counter++)
+	{
+		if (*static_cast<unsigned char *>(lhs) != *static_cast<unsigned char *>(rhs))
+			return false;
+	}
+	return true;
+}
+
+template <typename T>
+void swap(SecuredString<T> &lhs, SecuredString<T> &rhs)
+{
+	T *tmp_data = lhs.data;
+	lhs.data = rhs.data;
+	rhs.data = tmp_data;
+
+	bool tmp_owningData = lhs.owningData;
+	lhs.owningData = rhs.owningData;
+	rhs.owningData = tmp_owningData;
+
+	unsigned int tmp_datacnt = lhs.datacnt;
+	lhs.datacnt = rhs.datacnt;
+	rhs.datacnt = tmp_datacnt;
+}
+
+template <typename T>
+const SecuredString<T> SecuredString<T>::operator+(const SecuredString<T> &other) const {
+	SecuredString<T> result = *this;
+	result += other;
+	return result;
+}
+
+template <typename T>
+bool SecuredString<T>::operator == (const SecuredString<T> &other) const
+{
+	if (datacnt != other.datacnt)
+		return false;
+	return comparememory(reinterpret_cast<void *>(data), reinterpret_cast<void *>(other.data), datacnt*sizeof(T));
+}
+
+template<typename T>
+SecuredString<T> &SecuredString<T>::operator+=(const SecuredString<T> &rhs)
+{
+	SecuredString<T> tmp;
+	//create data
+	tmp.datacnt = datacnt + rhs.datacnt - 1;
+	tmp.data = new T[datacnt + rhs.datacnt - 1]; //only 1 padding \0
+	memcpy(tmp.data, data, (datacnt-1)*sizeof(T));
+	memcpy(tmp.data + (datacnt - 1), rhs.data, rhs.datacnt*sizeof(T));
+	
+	swap(*this, tmp);
+
+	return *this;
+}
+
+template<typename T>
+void SecuredString<T>::gainControl(T *data)
+{
+	SecuredString<T> tmp{};
+	swap<T>(*this, tmp);
+
+	this->data = data;
+	
+	//find data count
+	while (*data != 0x0)
+	{
+		this->datacnt++;
+		data++;
+	}
+
+	this->owningData = true;
+
+}
+
+template <typename T>
+SecuredString<T>::SecuredString()
+	: data{ nullptr }, datacnt{ 1 }, owningData{ false }
+{
+
+}
+
+//copy and swap idiom, together with combined operator=(const &) and operator=(&&)
+template <typename T>
+SecuredString<T> &SecuredString<T>::operator=(SecuredString rhs)
+{
+	swap(*this, rhs);
+	return *this;
+}
+
+template <typename T>
+SecuredString<T>::SecuredString(T *data)
+	:owningData{ true }, datacnt{ 1 } //cuz there is always at least a null character
+{
+	//find data count
+	T *tmp = data;
+	while (*tmp != 0x0)
+	{
+		datacnt++;
+		tmp++;
+	}
+
+	//create data
+	this->data = new T[datacnt];
+	memcpy(this->data, data, datacnt*sizeof(T));
+}
+
+template <typename T>
+SecuredString<T>::SecuredString(const SecuredString &rhs)
+	:data{ nullptr }, owningData{ rhs.owningData }, datacnt{rhs.datacnt}
+{
+	if (datacnt != 0)
+	{
+		//create data
+		data = new T[datacnt];
+		memcpy(data, rhs.data, datacnt*sizeof(T));
+	}
+}
+
+template <typename T>
+SecuredString<T>::SecuredString(SecuredString &&rhs)
+	:SecuredString{ rhs }
+{
+	rhs.data = nullptr;
+	rhs.owningData = false;
+}
+
+template <typename T>
+SecuredString<T>::~SecuredString()
+{
+	if (owningData)
+	{
+		SecureZeroMemory(data, datacnt*sizeof(T));
+		delete[] data;
+	}
+}
+
+template <typename T>
+T *SecuredString<T>::c_str() const
+{
+	return data;
+}
+
+template <typename T>
+T *SecuredString<T>::release()
+{
+	T *retval = data;
+	owningData = false;
+	data = nullptr;
+	return retval;
+}
+
+template < typename T >
+unsigned int SecuredString<T>::__get_length()
+{
+	return datacnt;
+}
+
+#endif
